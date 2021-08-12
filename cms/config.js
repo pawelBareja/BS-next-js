03deb8c492e58713b57c8e23e7eeeceff60ca3c0
pawelBareja <bareja.pawel@gmail.com>
2021-08-12 20:26:01 +0200

    .

--- cms/config.js
+++ cms/config.js
@@ -16,7 +16,6 @@ export default {
       fields: [
         { label: 'Title', name: 'title', widget: 'string' },
         { label: 'Description', name: 'description', widget: 'string' },
-        { label: 'Download', name: 'download', widget: 'string' },
         { label: 'Featured Image', name: 'thumbnail', widget: 'image' },
         { label: 'Author', name: 'author', widget: 'string', default: 'Admin' },
         { label: 'Body', name: 'body', widget: 'markdown' },
