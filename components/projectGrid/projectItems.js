fc0e433e3da2c0470d7dcd57b416ba8283abd175
pawelBareja <bareja.pawel@gmail.com>
2021-08-16 17:01:36 +0200

    config changes

--- components/projectGrid/projectItems.js
+++ components/projectGrid/projectItems.js
@@ -1,7 +1,7 @@
 export const projectItems = [
   { name: 'Metrostal Polska', type: 'corporate site', link: 'metrostal' },
-  { name: 'I Loft You', type: 'Shop', link: 'i-loft-you' },
-  { name: 'Cogen Technik', type: 'corporate site', link: 'cogen-technik' },
+  { name: 'I Loft You', type: 'Shop', link: 'iloftu' },
+  { name: 'Cogen Technik', type: 'corporate site', link: 'cogen' },
   { name: 'Viva Polonia!', type: 'corporate site', link: 'viva-polonia' },
   { name: 'StressLess', type: 'corporate site', link: 'stressless' },
   { name: 'Metrostal LTD', type: 'corporate site', link: 'metrostal-ltd' },
